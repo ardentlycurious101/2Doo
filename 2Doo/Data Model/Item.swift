f24b756a4219d596dee53bf7e383dc51f432603a
Elina <ardentlycurious101@gmail.com>
2019-07-23 23:28:58 -0700

    Convert to MVC Design Pattern.

---
+++ 2Doo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  2Doo
+//
+//  Created by Elina Lua Ming on 7/23/19.
+//  Copyright © 2019 Elina Lua Ming. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
