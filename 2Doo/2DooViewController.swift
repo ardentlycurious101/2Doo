f7a06a6dcfb62b74140faf95cf48e081a51c14bd
Elina <ardentlycurious101@gmail.com>
2019-07-25 16:29:23 -0700

    Implemented UISearchBarDelegate to query data in Core Data. Refactor loadItem() and extended _2DooViewController to implement UISearchBarDelegate.

--- 2Doo/2DooViewController.swift
+++ 2Doo/2DooViewController.swift
@@ -106,13 +106,32 @@ class _2DooViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request : NSFetchRequest<Item> = Item.fetchRequest()) {
+
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context : \(error)")
         }
+        
     }
+}
+
+// MARK: Search bar methods
+
+extension _2DooViewController : UISearchBarDelegate {
     
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        
+    }
+
 }
