d5f4432c8426b3ad6ede273937eff178f4257a5d
Elina <ardentlycurious101@gmail.com>
2019-07-26 14:01:24 -0700

    Go back to original list when searchBar is empty.

--- 2Doo/2DooViewController.swift
+++ 2Doo/2DooViewController.swift
@@ -114,6 +114,7 @@ class _2DooViewController: UITableViewController {
             print("Error fetching data from context : \(error)")
         }
         
+        tableView.reloadData()
     }
 }
 
@@ -131,6 +132,20 @@ extension _2DooViewController : UISearchBarDelegate {
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
         loadItems(with: request)
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+       
+        if searchBar.text?.count == 0 {
+           
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
         
     }
 
